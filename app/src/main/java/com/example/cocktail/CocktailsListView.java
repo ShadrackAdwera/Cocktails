ae747166e699fc7316e4394a744ecec54a11cbf8
ADWESH <adweshshaddie@gmail.com>
2020-01-31 15:51:32 +0300

    add a method to pass information from the home activity to the list view activity

--- app/src/main/java/com/example/cocktail/CocktailsListView.java
+++ app/src/main/java/com/example/cocktail/CocktailsListView.java
@@ -2,13 +2,25 @@ package com.example.cocktail;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CocktailsListView extends AppCompatActivity {
+
+
+    private TextView mResultsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cocktails_list_view);
+
+        mResultsTextView = findViewById(R.id.search_results);
+
+        Intent intent = getIntent();
+        String results = intent.getStringExtra("search");
+        mResultsTextView.setText("Cocktails Found for: "+results);
+
     }
 }
--- app/src/main/java/com/example/cocktail/WelcomeActivity.java
+++ app/src/main/java/com/example/cocktail/WelcomeActivity.java
@@ -2,13 +2,40 @@ package com.example.cocktail;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class WelcomeActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.search_cocktail) EditText searchCoctailText;
+    @BindView(R.id.btn_search_by_name) Button btnSearchByName;
+    @BindView(R.id.btn_search_by_ingredient) Button btnSearchByIngredient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        ButterKnife.bind(this);
+        btnSearchByName.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v){
+        if(v==btnSearchByName){
+            String search = searchCoctailText.getText().toString();
+            Intent intent = new Intent(WelcomeActivity.this, CocktailsListView.class);
+            intent.putExtra("search", search);
+            startActivity(intent);
+        } else if(v==btnSearchByIngredient){
+            String search = searchCoctailText.getText().toString();
+            Intent intent = new Intent(WelcomeActivity.this, CocktailsListView.class);
+            intent.putExtra("search", search);
+            startActivity(intent);
+        }
     }
 }
